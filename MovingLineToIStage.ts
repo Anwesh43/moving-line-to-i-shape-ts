f4a65ccd5b971d6f3370cf7c6c9ab019448a00da
Anwesh43 <anweshthecool0@gmail.com>
2019-05-02 15:42:22 +0530

    adding MovingLineToIStage class

--- MovingLineToIStage.ts
+++ MovingLineToIStage.ts
@@ -8,3 +8,34 @@ const foreColor : string = "#673AB7"
 const backColor : string = "#BDBDBD"
 const w : number = window.innerWidth
 const h : number = window.innerHeight
+
+class MovingLineToIStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : MovingLineToIStage = new MovingLineToIStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
