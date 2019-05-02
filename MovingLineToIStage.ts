25a4968bdca7c8ede1b0b79762d19033516f25e4
Anwesh43 <anweshthecool0@gmail.com>
2019-05-02 16:08:42 +0530

    adding MovingLineToI class

--- MovingLineToIStage.ts
+++ MovingLineToIStage.ts
@@ -186,3 +186,26 @@ class MLINode {
         return this
     }
 }
+
+class MovingLineToI {
+
+    curr : MLINode = new MLINode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
