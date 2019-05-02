69dd6a4a9c89e4583f723e6d785a61a3a0b4c9cb
Anwesh43 <anweshthecool0@gmail.com>
2019-05-02 15:39:46 +0530

    adding constants

---
+++ MovingLineToIStage.ts
@@ -0,0 +1,10 @@
+const nodes : number = 5
+const lines : number = 4
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const foreColor : string = "#673AB7"
+const backColor : string = "#BDBDBD"
+const w : number = window.innerWidth
+const h : number = window.innerHeight
